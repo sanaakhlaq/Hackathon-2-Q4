b9298f6ca2e78ded849575b1ebd76b125b563464
Your Name <your.email@example.com>
2026-02-10 08:18:26 +0500

    Updated Phase 4 with docker-compose and host fix

--- Phase4/main.py
+++ Phase4/main.py
@@ -101,4 +101,4 @@ async def chat_with_ai(request: AIChatRequest):
 
 # --- 7. Run Server ---
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=True)
